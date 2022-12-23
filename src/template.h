d62d8157828945ef5910c12a3547a708b7f93e5e
PatrykFlama <patrykflama@interia.pl>
2022-12-23 14:02:47 +0100

    bettercode template added

---
+++ src/template.h
@@ -0,0 +1,30 @@
+// #region
+/* --- LIBRARIES --- */
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stddef.h>
+#include <float.h>
+
+/* --- SUPERFOR --- */
+#define GET_MACRO_FOR(_1, _2, _3, _4, NAME, ...) NAME
+#define _FOR3(i, a, n, inc) for (auto i = (a); (inc) > 0 ? i < (n) : i >= (n); i += (inc))
+#define _FOR2(i, a, n) _FOR3(i, a, n, 1)
+#define _FOR1(i, n) _FOR2(i, 0, n)
+#define _FOR0(n) _FOR1(i, n)
+#define FOR(...) GET_MACRO_FOR(__VA_ARGS__, _FOR3, _FOR2, _FOR1, _FOR0)(__VA_ARGS__)    //? (/name/, //from//, to, ///inc///)
+
+/* --- VARS --- */
+#define ll long long int
+#define ld long double
+
+/* --- C BASIC CARE PACKAGE --- */
+#define max(a, b) (a > b ? a : b)
+#define min(a, b) (a < b ? a : b)
+
+/* --- MISC --- */
+#define nl putchar('\n')
+// #endregion
