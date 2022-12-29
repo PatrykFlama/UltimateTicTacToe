7a40b2cba4da73536408fd7be2025443a4c7788d
PatrykFlama <patrykflama@interia.pl>
2022-12-30 00:16:52 +0100

    game class: player updates

--- src/game.c
+++ src/game.c
@@ -5,15 +5,15 @@
 #include "game.h"
 
 // typedef struct Game{
-//     Player player1, player2;
+//     Player playero, playerx;
 //     char active_player;     // player, whos turn is now
 // } Game;
 
 
-void Game_init(Game *game, Player *_player1, Player *_player2, int game_size){   // start and initialize game
+void Game_init(Game *game, Player *_playero, Player *_playerx, int game_size){   // start and initialize game
     BigBoard_init(&(game->board), game_size);
-    game->player1 = _player1;
-    game->player2 = _player2;
+    game->playero = _playero;   // player 'o'
+    game->playerx = _playerx;   // player 'x'
     game->active_player = 'o';
 }
 
@@ -21,8 +21,13 @@ void Game_end(Game *game){    // end game
     BigBoard_delete(&(game->board));
 }
 
+Player* Game_give_player(Game *game){
+    if(game->active_player == 'o') return game->playero;
+    return game->playerx;
+}
+
 bool Game_player_move(Game *game){   // TODO: execute player move, true/false == success/or not
-    Move move = Player.move();
+    Move move = Player_move(Game_give_player(game));
     if(!Move_is_empty(&move)){     // if player made move
         return BigBoard_move_make(game->board, move.board, move.cell, game->active_player);
         Game_player_made_move(game);
--- src/game.h
+++ src/game.h
@@ -4,14 +4,14 @@
 #include "player.h"     // TODO
 
 typedef struct Game{
-    Player *player1, *player2;
+    Player *playero, *playerx;
     BigBoard board;
     char active_player;     // player, whos turn is now
     char game_won;
 } Game;
 
 //? ------ GAMEPLAY ------
-void Game_init(Game *game, Player *_player1, Player *_player2, int game_size);   // start and initialize game
+void Game_init(Game *game, Player *_playero, Player *_playerx, int game_size);   // start and initialize game
 void Game_end(Game *game);    // end game
 bool Game_player_move(Game *game);   // execute player move, true/false == success/or not
 void Game_player_made_move(Game *game);     // check if game won, switch active players
